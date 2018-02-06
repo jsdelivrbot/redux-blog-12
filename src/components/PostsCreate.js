8fc9b5bc5711835ee4b10c9170fb598295eac1e6
tylerwarren <tylerleewarren@gmail.com>
2018-02-05 21:24:03 -0500

    Route to index on cancel

--- src/components/PostsCreate.js
+++ src/components/PostsCreate.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsCreate extends Component {
 
@@ -47,6 +48,7 @@ class PostsCreate extends Component {
           component={this.renderField}
         />
         <button className="btn btn-primary" type="submit">Create</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
